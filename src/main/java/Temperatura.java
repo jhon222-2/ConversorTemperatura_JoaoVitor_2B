93a3fc83a9d18ae52cba9959fcb6fe386f951b69
2024105070038 <2024105070038@aqs-lab-c-01.mshome.net>
2025-04-24 14:37:17 -0400

    commit 3

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,4 +1,31 @@
 
 public class Temperatura {
-    
+    //ATRIBUTO PRIVADO PARA ARMAZENAR TEMP - CELSIUS
+        private double tempCelsius;
+        
+        //CONSTRUTOR - CRIAR O OBJETO COM UMA TEMP INICIAL
+        public Temperatura(double tempInicialCelsius){
+            this.tempCelsius = tempInicialCelsius;
+        }
+            
+            
+        //GETTER = PARALER A TEMPERATURA
+            public double
+                    getTemperaturaCelsius(){
+                        return tempCelsius;
+       }
+                    
+        //SETTER - PARA MUDAR A TEMPERATURA
+                    public void setTemperaturaCelsius(double mudarTempCelsius){
+                        this.tempCelsius = mudarTempCelsius;
+        }
+        //MÉTODO PARA CONVERTER CELSIUS PARA FAHRENHEIT
+               public double converteParaFahrenheit(){
+                  return (tempCelsius*9/5)+32; 
+        }
+               
+               //METODO PRA CONVERTER CESIUS PARA KELVIN
+               public double converteParaKelvin(){
+                   return tempCelsius+273.15;
+        }
 }
